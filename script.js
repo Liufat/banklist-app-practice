b60590524cebd9bade02ef39abd5860073c0e6e4
Liufat <dd5672003@gmail.com>
2023-02-23 15:12:16 +0800

    [新增]將account1.movements呈現在畫面上

--- script.js
+++ script.js
@@ -126,3 +126,24 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // currenciesUnique.forEach((v, i, set) => {
 //   console.log(`${i} : ${v}`);
 // });
+
+///////////////////////////////////////////////////////////////////////
+
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
