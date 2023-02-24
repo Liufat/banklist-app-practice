0c6544e1b0adae80c3a2b124aa8a8b6c7ceba0ab
Liufat <dd5672003@gmail.com>
2023-02-24 13:44:32 +0800

    [新稱]新稱使用者名稱功能(帳戶名的第一個字母)

--- script.js
+++ script.js
@@ -129,24 +129,36 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 ///////////////////////////////////////////////////////////////////////
 
-// const displayMovements = function (movements) {
-//   containerMovements.innerHTML = '';
+const displayMovements = function (movements) {
+  containerMovements.innerHTML = '';
 
-//   movements.forEach(function (mov, i) {
-//     const type = mov > 0 ? 'deposit' : 'withdrawal';
-//     const html = `
-//     <div class="movements__row">
-//           <div class="movements__type movements__type--${type}">${
-//       i + 1
-//     } ${type}</div>
-//           <div class="movements__value">${mov}</div>
-//         </div>
-//     `;
-//     containerMovements.insertAdjacentHTML('afterbegin', html);
-//   });
-// };
+  movements.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+    <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+    `;
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
 
-// displayMovements(account1.movements);
+displayMovements(account1.movements);
+
+const createUsername = accounts => {
+  accounts.forEach(account => {
+    account.username = account.owner
+      .toLowerCase()
+      .split(' ')
+      .map(v => v[0])
+      .join('');
+  });
+};
+createUsername(accounts);
+console.log(accounts);
 
 ///////////coding test//////////////////
 
